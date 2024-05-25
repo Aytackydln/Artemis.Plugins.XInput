98522fe1a5ffbc78365bd1619df85675a81c474d
Aytackydln <aytac.kayadelen@gmail.com>
2024-05-26 01:12:19 +0200

    update descr and add thumb values

--- src/Artemis.Plugins.XInputData/ControllerNode.cs
+++ src/Artemis.Plugins.XInputData/ControllerNode.cs
@@ -15,4 +15,10 @@ public class ControllerNode(int userIndex)
 
     public byte LeftTrigger => XInput.GetState(userIndex, out _state) ? _state.Gamepad.LeftTrigger : (byte)0;
     public byte RightTrigger => XInput.GetState(userIndex, out _state) ? _state.Gamepad.RightTrigger : (byte)0;
+
+    public short LeftThumbX => XInput.GetState(userIndex, out _state) ? _state.Gamepad.LeftThumbX : (short)0;
+    public short LeftThumbY => XInput.GetState(userIndex, out _state) ? _state.Gamepad.LeftThumbY : (short)0;
+
+    public short RightThumbX => XInput.GetState(userIndex, out _state) ? _state.Gamepad.RightThumbX : (short)0;
+    public short RightThumbY => XInput.GetState(userIndex, out _state) ? _state.Gamepad.RightThumbY : (short)0;
 }
